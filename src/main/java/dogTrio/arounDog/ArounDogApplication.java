d9864f61b44fc93a06323a3aadc47e988ff4a84c
hyeonmin <hyeonmin97@naver.com>
2022-09-07 16:40:50 +0900

    init project + basic setting

---
+++ src/main/java/dogTrio/arounDog/ArounDogApplication.java
@@ -0,0 +1,13 @@
+package dogTrio.arounDog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArounDogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArounDogApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dogTrio/arounDog/ArounDogApplicationTests.java
@@ -0,0 +1,13 @@
+package dogTrio.arounDog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArounDogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
