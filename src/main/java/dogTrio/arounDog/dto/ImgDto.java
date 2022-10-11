983188b24d611c36a13deb66ba9576f477f26367
hyeonmin <hyeonmin97@naver.com>
2022-10-11 20:34:24 +0900

    modify ImgDto
    ImgDto에 id 추가, 트랜잭션 변경

--- src/main/java/dogTrio/arounDog/dto/ImgDto.java
+++ src/main/java/dogTrio/arounDog/dto/ImgDto.java
@@ -6,10 +6,12 @@ import java.util.Optional;
 
 @Getter
 public class ImgDto {
+    private Long id;
     private String path;
     private Optional<byte[]> img;
 
-    public ImgDto(String path, Optional<byte[]> img) {
+    public ImgDto(Long id, String path, Optional<byte[]> img) {
+        this.id = id;
         this.path = path;
         this.img = img;
     }
--- src/main/java/dogTrio/arounDog/service/DogImgService.java
+++ src/main/java/dogTrio/arounDog/service/DogImgService.java
@@ -5,9 +5,9 @@ import dogTrio.arounDog.dto.ImgDto;
 import dogTrio.arounDog.repository.DogImgRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import javax.imageio.ImageIO;
-import javax.transaction.Transactional;
 import java.awt.image.BufferedImage;
 import java.io.ByteArrayOutputStream;
 import java.io.File;
@@ -18,20 +18,22 @@ import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
+@Transactional(readOnly = true)
 public class DogImgService {
 
     private final DogImgRepository dogImgRepository;
 
-    @Transactional
+
     public List<ImgDto> getImgList(Long dogId) {
         List<ImgDto> imgList = new ArrayList<>();
         List<DogImg> findDogImgs = dogImgRepository.findDogImgs(dogId);//이미지 경로 불러오기
 
         for (DogImg dogImg : findDogImgs) {
+            Long id = dogImg.getId();
             String path = dogImg.getPath();
             Optional<byte[]> img = getImg(path);//경로에 있는 이미지 저장
 
-            ImgDto imgDto = new ImgDto(path, img);
+            ImgDto imgDto = new ImgDto(id, path, img);
             imgList.add(imgDto);
         }
         return imgList;
