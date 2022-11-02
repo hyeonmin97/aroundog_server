60a3875536573c82c0701755d1841b24fb6bf677
hyeonmin <hyeonmin97@naver.com>
2022-11-03 02:59:12 +0900

    Feat: update dogBreed
    강아지 종류 추가

--- src/main/java/dogTrio/arounDog/domain/DogBreed.java
+++ src/main/java/dogTrio/arounDog/domain/DogBreed.java
@@ -1,5 +1,23 @@
 package dogTrio.arounDog.domain;
 
 public enum DogBreed {
-    BIG, MEDIUM, SMALL
+    DOGLENGTH,
+    HUSKY,
+    SAMOYED,
+    RETRIEVER,
+    SHEPHERD,
+    MALAMUTE,
+    BEAGLE,
+    BORDERCOLLIE,
+    BULLDOG,
+    SHIBA,
+    WELSHCORGI,
+    CHIHUAHUA,
+    MALTESE,
+    POODLE,
+    SHIHTZU,
+    YORKSHIRETERRIER,
+    DOGBITECT,
+    DOGMEDIUMECT,
+    DOGSMALLECT,
 }
